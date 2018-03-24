527703f85f68a42ead593bda470db5f4008c39fc
valeria campilongo <cvaleriac@hotmail.it>
2018-03-24 21:17:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,10 @@ class Dog
   def initialize(name)
     @name = name
   end
+  
+  def name=(name)
+    @name = name
+  end
 
   def name
     @name
@@ -11,15 +15,12 @@ class Dog
   def initialize(breed)
     @breed = breed
   end
-
+ 
   def breed=(breed)
     @breed = breed
   end
-
+ 
   def breed
     @breed
   end
 end
-lassie = Dog.new("Collie")
-lassie.name
-lassie.breed
